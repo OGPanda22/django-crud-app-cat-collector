134ff714a346b859c269628671b74807748728f8
zlibres22 <zacklibres@gmail.com>
2025-07-24 21:14:40 -0400

    database

---
+++ main_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .models import Cat
+
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+
+# # views.py
+
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
+
+# # Create a list of Cat instances
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
+
+def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
